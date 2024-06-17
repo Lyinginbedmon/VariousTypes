6092d8fdeab409c11ee2576abec2c4d5cdcac9dc
Lying <7865059+Lyinginbedmon@users.noreply.github.com>
2024-06-17 21:04:19 +0100

    Initial commit

---
+++ common/src/main/java/com/lying/ExampleMod.java
@@ -0,0 +1,9 @@
+package com.lying;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "vartypes";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ neoforge/src/main/java/com/lying/neoforge/ExampleModNeoForge.java
@@ -0,0 +1,13 @@
+package com.lying.neoforge;
+
+import net.neoforged.fml.common.Mod;
+
+import com.lying.ExampleMod;
+
+@Mod(ExampleMod.MOD_ID)
+public final class ExampleModNeoForge {
+    public ExampleModNeoForge() {
+        // Run our common setup.
+        ExampleMod.init();
+    }
+}
